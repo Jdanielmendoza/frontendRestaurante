9ec9bd6c67398c4af3eff447975d40ca4f101a7d
Jose Daniel Mendoza <jdanielmendoza987@gmail.com>
2023-10-12 01:29:49 -0400

    test Daniel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
-
+import icono from "/icono.svg"
 const Sidebar = ()=>{
     return(
         <>
             <p>Barra Lateral</p>
+            <img src={icono} alt="icono" />
         </>
     )
 }
