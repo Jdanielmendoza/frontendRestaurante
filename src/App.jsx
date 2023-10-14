bbc81651816a7a289ebce75367dfd162171ed04a
= <pumaronal2016@gmail.com>
2023-10-14 15:45:49 -0400

    Agrege el dashboard y modifique y agregue el panel de registrar usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-import Sidebar from './components/Sidebar.jsx';
-
+import Dashboardview from './components/Dashboardview';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Sidebar/>
-    </>
+    <div className='flex'>
+      <div className='basis-[12%] h-[100vh]'>
+        <Sidebar/>
+      </div>
+
+      <div className='basis-[88%] border'>
+        <Dashboardview/>
+      </div>
+    </div>
   )
 }
 
