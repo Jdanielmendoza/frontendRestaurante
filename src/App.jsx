d3e637dfa1cf196656042275c2a4c27597b251d3
Carlos Diego Marca Peñaranda <marcapenaranda@gmail.com>
2023-10-22 19:58:14 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Sidebar from './components/Sidebar.jsx';
 import FormularioLogin from './components/FormularioLogin/FormularioLogin';
 import CambiarPassword from './components/cambiarPassword/cambiarPassword';
+import PaymentType from './components/PaymentType/PaymentType';
 function App() {
   return (
     <>
       {/* <Sidebar/> */}
       {/* <FormularioLogin/> */}
-      <CambiarPassword/>
-    </>
+      {/*<CambiarPassword/>*/}   
+      <PaymentType/>
+      </>
   )
 }
 export default App
---
+++ src/components/PaymentType/PaymentType.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./PaymentType.css";
+const PaymentType = () => {
+  return (
+    <div className="contenedorTipoDePago">
+      <div className="tituloTipoDePago">TIPO DE PAGO</div>
+      <div className="contenedorLabelInputTextSubmit">
+        <label htmlFor="inputTextId" className="labelTipoDePago">
+          NUEVO TIPO DE PAGO
+        </label>
+        <input
+          type="text"
+          className="inputText"
+          id="inputTextId"
+          placeholder="Nombre Del Tipo De Pago"
+        ></input>
+        <input type="submit" 
+        value={AGREGAR} 
+        className="inputAgregar"
+        ></input>
+      </div>
+    </div>
+  );
+};
+export default PaymentType;
